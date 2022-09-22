77465485d089f623254458e6756f4cd02bb2b2bb
kuzmychAndrew <89782153+kuzmychAndrew@users.noreply.github.com>
2022-09-22 15:35:12 +0300

    Initial Commit

---
+++ YourWeather/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  YourWeather
+//
+//  Created by Андрій Кузьмич on 22.09.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ YourWeather/YourWeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  YourWeatherApp.swift
+//  YourWeather
+//
+//  Created by Андрій Кузьмич on 22.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct YourWeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
